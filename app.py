40b9c307eced6976376e5bed2fab55502bb92999
Ntandoyenkosi Matshisela <drmatshisela@gmail.com>
2024-02-27 22:28:15 +0000

    add dynamic data and api

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
@@ -31,6 +31,10 @@ def hello_world():
                          jobs=JOBS,
                         company_name='Jovian')
 
+
+@app.route('/api/jobs')
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=8080, debug=True)
